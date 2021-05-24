f82e025afaf66bba595dc19dc07530c270ead5f2
CesarArellano <raywayday@gmail.com>
2021-05-24 15:33:21 -0500

    Doing CounterApp homework

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+// import PrimeraApp from './PrimeraApp';
 import './index.css';
+
 
 const divApp = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo="Hola, soy César"/>, divApp);
+ReactDOM.render(<CounterApp value={1111} />, divApp);
