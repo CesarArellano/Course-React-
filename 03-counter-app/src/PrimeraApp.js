8b56584e898d557ecf4735d208b46b8b2bb94520
CesarArellano <raywayday@gmail.com>
2021-05-24 14:02:26 -0500

    Printing variables in HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,13 +2,20 @@
 // Functional Components
 
 const PrimeraApp = () => {
+  const saludo = 'Hola Mundo const';
+  const objeto = {
+    nombre: 'César',
+    edad: 21
+  };
+
   return (
     // <Fragment>
     //   <h1>Hola Mundo</h1>
     //   <p>Hola</p>
     // </Fragment>
     <>
-      <h1>Hola Mundo</h1>
+      <h1>{ saludo }</h1>
+      {/* <pre>{ JSON.stringify(objeto, null, 2)}</pre> */}
       <p>Hola</p>
     </>    
   );
