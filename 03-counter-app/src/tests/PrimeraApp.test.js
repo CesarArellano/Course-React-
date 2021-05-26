1d011f3c7c105f32566d9b60e7f467370c9e84d2
CesarArellano <raywayday@gmail.com>
2021-05-26 18:09:59 -0500

    Knowing and installing Enzyme

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
+// import { render } from '@testing-library/react';
+
 import PrimeraApp from '../PrimeraApp';
 
 describe('Tests in <PrimerApp/>', () => {
-  test('Must show the message "Hola, Soy Gokú"', () => {
-    const greetings = 'Hola, soy Gokú';
-    const { getByText } = render(<PrimeraApp saludo={ greetings } />);
-    expect( getByText(greetings) ).toBeInTheDocument();
-  })
-  
-})
+  // test('Must show the message "Hola, Soy Gokú"', () => {
+  //   const greetings = 'Hola, soy Gokú';
+  //   const { getByText } = render(<PrimeraApp saludo={ greetings } />);
+  //   expect( getByText(greetings) ).toBeInTheDocument();
+  // })
+
+  test('Must be show <PrimerApp /> correctly', () => {
+    const saludo = 'Hola, soy César';
+    const wrapper = shallow(<PrimeraApp saludo={ saludo } />)
+    expect(wrapper).toMatchSnapshot();
+  });
+});
