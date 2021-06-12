712e438de92b4dcd3c869452118cb22d70673018
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-11 19:12:21 -0500

    Testing GifGridItem Component

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -5,11 +5,33 @@ import { GifGridItem } from '../../components/GifGridItem.js';
 describe('Tests with GifGridItem Component', () => {  
   const title = 'Título de prueba';
   const url = 'https://localhost/ejemplo.png'
+  const wrapper = shallow(<GifGridItem 
+      title={title}
+      url={ url } />);
+  
   test('Must show <GifGridItem /> correctly', () => {
-    const wrapper = shallow(<GifGridItem 
-        title={title}
-        url={ url } />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Must display a paragraph with a title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('Must display an image equal to the url and alt of the props', () => {
+    const img = wrapper.find('img');
+    const imgProps = img.props();
+    // expect(img.prop('url').src.trim()).toBe(url);
+    expect(imgProps.src.trim()).toBe(url);
+    expect(imgProps.alt.trim()).toBe(title);
+  });
+
+  test('Must have animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    const classNameDiv = div.prop('className');
+    const isIncluded = classNameDiv.includes('animate__fadeIn');
+    expect(isIncluded).toBe(true);
+    // expect(isIncluded).not.toBe(true);
   });
 });
 
