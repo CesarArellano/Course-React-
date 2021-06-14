a6482022e784c902cde68524ec7d0767ee009864
CesarArellano <raywayday@gmail.com>
2021-06-13 19:04:28 -0500

    Testing handleInputChange

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -4,8 +4,16 @@ import { AddCategory } from '../../components/AddCategory';
 describe('Testing AddCategory Component', () => {
   const setCategories = () => {};  
   const wrapper = shallow(<AddCategory setCategories={ setCategories } />);
+  
   test('Must display correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Must change the text box', () => {
+    const input = wrapper.find('input');
+    const value = 'Hello World';
+    input.simulate('change', { target: { value } });
+    expect(wrapper.find('p').text().trim()).toBe(value)
   });
   
 });
