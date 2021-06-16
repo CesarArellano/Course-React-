532cfbcd2fcb0f2061e98fb17ff968ab5fc47cfd
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-16 13:52:13 -0500

    Knowing waitForNextUpdate

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -2,13 +2,20 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('Testing with useFetchGifs Hook', () => {
-  test('Must return initial state', () => {
-    const { result } = renderHook(() => useFetchGifs('One Punch'));
+  test('Must return initial state', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('One Punch'));
     const { data, loading } = result.current;
-
-    console.log(data, loading);
+    await waitForNextUpdate(); // This function does a cleaning of the state, so that in the next test we can obtain the expected values.  console.log(data, loading);
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test('Must return images array and loading in false', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('One Punch'));
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
   
 });
