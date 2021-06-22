fbd93f40a870c550ab6d9775ca2348332633284e
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-22 14:16:58 -0500

    Creating Form with Custom Hook

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <SimpleForm />,
+  <FormWithCustomHook />,
   document.querySelector('#root')
 );
