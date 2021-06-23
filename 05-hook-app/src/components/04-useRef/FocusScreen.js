ef26e4cbe4acf1335602961ee6aa85738be511c3
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-23 12:53:10 -0500

    Knowing useRef

---
+++ 05-hook-app/src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import '../../effects.css';
+
+export const FocusScreen = () => {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.select();
+  };
+
+  return (
+    <div>
+      <h1>Focus Screen</h1>
+      <hr />
+      <input
+        ref={ inputRef }
+        className="form-control"
+        placeholder="Su nombre"
+      />
+      <button 
+        className="btn btn-outline-primary mt-4"
+        onClick={ handleClick }
+      >
+        Focus
+      </button>
+    </div>
+  )
+}
