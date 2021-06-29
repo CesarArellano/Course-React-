c041541becd50244abd591a561b7f86b94ea333a
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-29 17:36:28 -0500

    Creating the shell from the TODOs list

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -5,7 +5,7 @@ import './styles.css';
 
 const initialState = [{
   id: new Date().getTime(),
-  desc: 'Aprender React',
+  desc: 'Learn React',
   done: false
 }];
 
@@ -15,13 +15,48 @@ export const TodoApp = () => {
   console.log(todos);
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({ todos.length }) </h1>
       <hr/>
-      <ul>
-        <li>Hola</li>
-        <li>Mundo</li>
-        <li>De nuevo</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+        <ul className="list-group list-group-flush">
+          {
+            todos.map( (todo, i) => {
+              return ( <li
+                key={ todo.id }
+                className="list-group-item"
+              >
+                <p className="text-center complete">{i + 1}. { todo.desc }</p>
+                <button
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </li>
+              );
+            })
+          }
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add TODO</h4>
+          <hr/>
+          <form>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder='Learn ...'
+              autoComplete='off'
+            />
+            <div className="d-grid mt-2">
+              <button className="btn btn-outline-primary" type="button">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
