912b70cfe90d17e738a1643c07eb9d430fc1796e
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-29 18:49:03 -0500

    Adding toggle action

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,18 +1,60 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm';
 
 import './styles.css';
 
-const initialState = [{
-  id: new Date().getTime(),
-  desc: 'Learn React',
-  done: false
-}];
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
-  const [ todos ] = useReducer(todoReducer, initialState);
-  console.log(todos);
+  const [ todos, dispatch ] = useReducer(todoReducer, [], init);
+  
+  const [ { description }, handleInputChange, reset ] = useForm({ description: '' });
+  
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleDelete = (todoId) => {
+    const action = {
+      type: 'delete',
+      payload: todoId
+    }
+
+    dispatch(action);
+  };
+
+  const handleToogle = (todoId) => {
+    dispatch({
+      type: 'toggle',
+      payload: todoId
+    });
+  }
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    
+    if(description.trim().length <= 1) return;
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false
+    };
+
+    const action = {
+      type: 'add',
+      payload: newTodo
+    }
+
+    dispatch(action);
+    reset();
+  }
+
   return (
     <div>
       <h1>TodoApp ({ todos.length }) </h1>
@@ -26,9 +68,15 @@ export const TodoApp = () => {
                 key={ todo.id }
                 className="list-group-item"
               >
-                <p className="text-center complete">{i + 1}. { todo.desc }</p>
+                <p
+                  className={ `${ todo.done && 'complete'}`}
+                  onClick={ () => handleToogle(todo.id) }
+                >
+                  {i + 1}. { todo.desc }
+                </p>
                 <button
                   className="btn btn-danger"
+                  onClick={() => handleDelete( todo.id ) }
                 >
                   Delete
                 </button>
@@ -41,16 +89,18 @@ export const TodoApp = () => {
         <div className="col-5">
           <h4>Add TODO</h4>
           <hr/>
-          <form>
+          <form onSubmit={ handleSubmit }>
             <input
               type="text"
               name="description"
               className="form-control"
               placeholder='Learn ...'
               autoComplete='off'
+              onChange={ handleInputChange }
+              value={ description }
             />
             <div className="d-grid mt-2">
-              <button className="btn btn-outline-primary" type="button">
+              <button className="btn btn-outline-primary" type="submit">
                 Add
               </button>
             </div>
