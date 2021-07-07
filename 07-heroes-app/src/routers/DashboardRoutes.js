4d62a70698305f51e0aa3f956cf80ddd05191ef1
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-07-07 13:39:57 -0500

    Creating Heroes List

--- 07-heroes-app/src/routers/DashboardRoutes.js
+++ 07-heroes-app/src/routers/DashboardRoutes.js
@@ -3,17 +3,17 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { Navbar } from '../components/ui/Navbar';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { HeroeScreen } from '../components/heroes/HeroeScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
 import { DcScreen } from '../components/dc/DcScreen';
 
 export const DashboardRoutes = () => {
   return (
     <>
       <Navbar />
-      <div>
+      <div className="container mt-3">
         <Switch>
           <Route exact path="/marvel" component={ MarvelScreen } />
-          <Route exact path="/heroe/:heroeId" component={ HeroeScreen } />
+          <Route exact path="/heroe/:heroeId" component={ HeroScreen } />
           <Route exact path="/dc" component={ DcScreen } />
           <Redirect to="/marvel" />
         </Switch>
