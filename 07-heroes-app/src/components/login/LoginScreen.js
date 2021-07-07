093ca94d5f299e3e6ad389cd99beac0f9b8dc0de
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-07-07 17:36:09 -0500

    Implementing Private Routes

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
-
+  const { dispatch } = useContext(AuthContext);
   const handleLogin = () => {
-    // history.push('/');
+    
+    const newLogin = {
+      type: types.login,
+      payload: {
+        name: 'César Arellano'
+      }
+    };
+    
+    dispatch(newLogin);
     history.replace('/');
   }
 
