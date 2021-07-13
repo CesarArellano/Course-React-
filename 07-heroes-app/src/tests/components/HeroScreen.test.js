ce60bd1b4b4983935a14960c6e71f75692379620
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-07-13 17:37:21 -0500

    Simulating params in the url

--- 07-heroes-app/src/tests/components/HeroScreen.test.js
+++ 07-heroes-app/src/tests/components/HeroScreen.test.js
@@ -1,5 +1,5 @@
 import { mount } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { HeroScreen } from '../../components/heroes/HeroScreen';
 
 describe('Testing Hero Screen Component', () => {
@@ -7,17 +7,74 @@ describe('Testing Hero Screen Component', () => {
     length: 10,
     push: jest.fn(),
     goBack: jest.fn()
-
   }
+  
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  const wrapper = mount(
-    <MemoryRouter initialEntries={ ['/hero']}>
-      <HeroScreen history={ history}/>
-    </MemoryRouter>
-  );
-  
   test('Must show correctly', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={ ['/hero']}>
+        <HeroScreen history={ historyMock }/>
+      </MemoryRouter>
+    );
     expect(wrapper.find('Redirect').exists()).toBe(true);
-  })
+  });
+
+  test('should show you a hero if the param exists and finds it', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={ ['/hero/marvel-spider'] }>
+        <Route path="/hero/:heroId" component={ HeroScreen } />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('.row').exists()).toBe(true);
+  });
   
+  test('should go back to the last screen with PUSH', () => {
+    const historyMock = {
+      length: 1,
+      push: jest.fn(),
+      goBack: jest.fn()
+    }
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={ ['/hero/marvel-spider'] }>
+        <Route path="/hero/:heroId" component={ (props) => <HeroScreen history={ historyMock } /> } />
+      </MemoryRouter>
+    );
+    wrapper.find('button').prop('onClick')();
+    expect(historyMock.push).toHaveBeenCalledWith('/');
+    expect(historyMock.goBack).not.toHaveBeenCalled();
+  });
+
+  test('should go back to the last screen with GoBack', () => {
+    const historyMock = {
+      length: 3,
+      push: jest.fn(),
+      goBack: jest.fn()
+    }
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={ ['/hero/marvel-spider'] }>
+        <Route path="/hero/:heroId" component={ () => <HeroScreen history={ historyMock } /> } />
+      </MemoryRouter>
+    );
+    wrapper.find('button').prop('onClick')();
+    expect(historyMock.push).toHaveBeenCalledTimes(0);
+    expect(historyMock.goBack).toHaveBeenCalled();
+  });
+
+  test('should call Redirect if hero does not exist', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={ ['/hero/marvel-spider23423'] }>
+        <Route 
+          path="/hero/:heroId" 
+          component={ () => <HeroScreen history={ historyMock } /> } 
+        />
+      </MemoryRouter>
+    );
+    expect(wrapper.text()).toBe('');
+  });
+
 })
