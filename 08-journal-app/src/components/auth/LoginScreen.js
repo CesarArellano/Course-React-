67bf413c81349eb144e1469f97e2ad545c330359
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-07-14 18:23:03 -0500

    applying CSS styles to AuthRouter

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className="auth__title">Login</h3>
       <form>
         <input
           type="text"
@@ -24,7 +25,22 @@ export const LoginScreen = () => {
           Login
         </button>
         <hr />
-        Google
+        <div>
+          <p>Login with social networks</p>
+          <div className="google-btn">
+              <div className="google-icon-wrapper">
+                  <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+              </div>
+              <p className="btn-text">
+                  <b>Sign in with google</b>
+              </p>
+          </div>
+        </div>
+          <Link 
+            to="/auth/register"
+          >
+            Create New Account
+          </Link>
       </form>
     </>
   )
