c1b9f7ec661bce91edc287a9d96112c5acfe2612
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-08-14 19:39:20 -0500

    Designing Calendar Screen

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Navbar } from '../ui/Navbar'
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import { Navbar } from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+  title: 'Register Social Service',
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgcolor: '#fafafa',
+}];
 
 export const CalendarScreen = () => {
   return (
-    <>
+    <div className="calendar-screen">
       <Navbar />
-      <h1>Calendar Screen</h1>
-    </>
+      <div class="custom-container">
+        <h1>Calendar Screen</h1>
+        <Calendar
+          localizer={ localizer }
+          events={ events }
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
+    </div>
   )
 }
