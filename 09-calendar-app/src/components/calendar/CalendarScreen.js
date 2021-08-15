3b6c94624cabeddc867339577cf739d3119da21b
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-08-14 19:17:40 -0500

    Adding login.css and Navbar.js

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Navbar } from '../ui/Navbar'
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <h1>Calendar Screen</h1>
-    </div>
+    </>
   )
 }
---
+++ 09-calendar-app/src/components/ui/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">
+        César
+      </span>
+      <button className="btn btn-outline-danger">
+        <span class="textButton">Salir</span>
+        <i className="fas fa-sign-out-alt"></i>
+      </button>
+    </div>
+  )
+}
