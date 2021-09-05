62ca4b4e6fce11201d55b8dfd4151edc840d3d67
CesarArellano <raywayday@gmail.com>
2021-09-05 13:25:40 -0500

    Fixing modal close timeout

--- 09-calendar-app/src/components/calendar/CalendarModal.js
+++ 09-calendar-app/src/components/calendar/CalendarModal.js
@@ -15,10 +15,9 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
-  const [isOpen, setIsOpen] = useState(true)
-  
+  const [isOpen, setIsOpen] = useState(true);
   const closeModal = () => {
-    setIsOpen(false);
+    setIsOpen(false)
   }
 
   return (
@@ -28,13 +27,55 @@ export const CalendarModal = () => {
         //onAfterOpen={ afterOpenModal }
         onRequestClose={ closeModal }
         style={ customStyles }
-        closeTimeoutMS="300"
+        closeTimeoutMS= { 200 }
         className="modal"
         overlayClassName="modal-fondo"
       >
-        <h1>Hello World</h1>
+        <h1 className="modalTitle"> Nuevo evento </h1>
         <hr />
-        <span>Hola de nuevo...</span>
+        <form className="container">
+          <div className="form-group">
+            <label>Fecha y hora inicio</label>
+            <input className="form-control" placeholder="Fecha inicio" />
+          </div>
+
+          <div className="form-group">
+            <label>Fecha y hora fin</label>
+            <input className="form-control" placeholder="Fecha inicio" />
+          </div>
+
+          <hr />
+          <div className="form-group">
+            <label>Título y notas</label>
+            <input 
+              type="text" 
+              className="form-control"
+              placeholder="Título del evento"
+              name="title"
+              autoComplete="off"
+            />
+            <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+          </div>
+
+          <div className="form-group">
+            <textarea 
+              type="text" 
+              className="form-control"
+              placeholder="Notas"
+              rows="4"
+              name="notes"
+            ></textarea>
+            <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-outline-primary btn-block"
+          >
+            <i className="far fa-save"></i>
+            <span> Guardar</span>
+          </button>
+        </form>
       </Modal>
     </div>
   )
