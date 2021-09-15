94449c6736eda892e71817a568b03bcadb37838b
CesarArellano <raywayday@gmail.com>
2021-09-14 23:19:36 -0500

    Adding DeletingEventFab Component

---
+++ 09-calendar-app/src/components/ui/DeletingEventFab.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { eventDelete, eventUpdate } from '../../actions/events';
+
+export const DeletingEventFab = () => {
+  const dispatch = useDispatch();
+  const { actionEvent } = useSelector(state => state.calendar)
+
+  const handleDeleteEvent = () => {
+    dispatch( eventDelete( actionEvent ) );
+  }
+
+  return (
+    <button
+      className="btn btn-danger fab-danger"
+      onClick={ handleDeleteEvent }
+    >
+      <i className="fas fa-trash"></i>
+      <span> Borrar evento</span>
+    </button>
+  )
+}
