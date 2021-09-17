c827fcfce2d483c73931c49dc644787d60edd361
CesarArellano <raywayday@gmail.com>
2021-09-17 00:30:46 -0500

    Setting express server

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,18 @@
-console.log('Hola Dev');
+const { response } = require('express');
+const express = require('express');
+
+// Create express server
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  console.log('Se solicitó el /');
+  res.json({
+    ok: true
+  })
+});
+
+// Listen to requests
+app.listen( 4000, () => {
+  console.log(`Servidor corriendo en puerto ${ 4000 }`);
+})
