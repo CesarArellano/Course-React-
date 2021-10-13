0d3c7a38e159c08f1917a92f1dd6928528f3a5a5
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-12 19:57:33 -0500

    Adding validateFields middleware

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,19 +1,17 @@
 const { response } = require('express');
 const { validationResult } = require('express-validator');
 
-const errorJson = ( res, errors ) => {
-  return res.status(400).json({
-    ok: false,
-    errors: errors.mapped()
-  });
-}
-
 const createUser = ( req, res = response ) => {
   const { name, email, password } = req.body;
 
   const errors = validationResult( req );
   
-  if( !errors.isEmpty() ) return errorJson(res, errors);
+  if( !errors.isEmpty() )  {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
 
   res.status(201).json({
     ok: true,
@@ -21,7 +19,8 @@ const createUser = ( req, res = response ) => {
     name,
     email,
     password
-  })
+  });
+
 }
 
 const userLogin = ( req, res = response ) => {
@@ -36,18 +35,19 @@ const userLogin = ( req, res = response ) => {
     msg: 'login',
     email,
     password
-  })
-}
+  });
+
+};
 
 const revalidateToken = ( req, res = response ) => {
   res.json({
     ok: true,
     msg: 'renew'
-  })
-}
+  });
+};
 
 module.exports = {
   createUser,
   userLogin,
   revalidateToken
-}
+};
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -7,14 +7,16 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const router = Router();
 
+const { validateFields } = require('../middlewares/validate-fields');
 const { createUser, userLogin, revalidateToken } = require('../controllers/auth');
 
 router.post(
   '/new',
-  [
+  [ // middlewares
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    validateFields,
   ],
   createUser
 );
@@ -24,6 +26,7 @@ router.post(
   [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    validateFields,
   ],
   userLogin
 );
