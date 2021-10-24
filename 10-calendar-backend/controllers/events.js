53207b454e08e7bf1dd493bb836b9c4e7e665683
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-24 18:51:59 -0500

    Saving events in database

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 const getEvents = ( req, res = response ) => {
 
@@ -8,12 +9,26 @@ const getEvents = ( req, res = response ) => {
   });
 };
 
-const createEvent = ( req, res = response ) => {
+const createEvent = async ( req, res = response ) => {
   // Verificar el evento.
-  res.json({
-    ok: true,
-    msg: 'createEvent'
-  });
+
+  const event = new Event( req.body );
+
+  try {
+    event.user = req.uid;
+    
+    const savedEvent = await event.save();
+    res.json({
+      ok: true,
+      event: savedEvent,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Talk to the admin'
+    });
+  }
+
 };
 
 const updateEvent = ( req, res = response ) => {
