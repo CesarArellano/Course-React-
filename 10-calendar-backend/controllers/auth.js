e511eb86e33ff75bf3452caa4de2aa4bf1d6aeaf
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-24 13:37:48 -0500

    Adding create user in mongodb

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,16 +1,24 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = ( req, res = response ) => {
-  const { name, email, password } = req.body;
-
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password
-  });
-
+const createUser = async ( req, res = response ) => {
+  
+  try {
+    const user = new User( req.body );
+    user.save();
+    
+    res.status(201).json({
+      ok: true,
+      msg: 'register'
+    });
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please talk to the admin'
+    });
+  }
+  
 }
 
 const userLogin = ( req, res = response ) => {
