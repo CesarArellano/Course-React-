32e50f76d2a5d7e0c6778bcdc73ee0189bb0c53c
GtecMB_MQ6L4 <gtecmb_mq6l4@MacBook-Air-de-GtecMB-MQ6L4.local>
2021-10-30 17:46:12 -0500

    Setting authReducer

---
+++ 09-calendar-app/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+const initState = {
+  checking: true,
+}
+
+export const authReducer = ( state = initState, action ) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- 09-calendar-app/src/types/types.js
+++ 09-calendar-app/src/types/types.js
@@ -1,5 +1,14 @@
 
 export const types = {
+
+  authChecking: '[AUTH] Checking login state',
+  authCheckingFinish: '[AUTH] Finish checking login state',
+  authStartLogin: '[AUTH] Start login',
+  authLogin: '[AUTH] Login',
+  authStartRegister: '[AUTH] Start register',
+  authStartTokenRenew: '[AUTH] Start token renew',
+  authLogout: '[AUTH] Logout',
+
   uiOpenModal: '[UI] Open modal',
   uiCloseModal: '[UI] Close modal',
 
