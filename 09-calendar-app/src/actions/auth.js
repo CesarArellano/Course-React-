c1acf71747c939f8f7046789899f695915684241
CesarArellano <raywayday@gmail.com>
2021-10-30 18:59:34 -0500

    Making http request to auth

--- 09-calendar-app/src/actions/auth.js
+++ 09-calendar-app/src/actions/auth.js
@@ -1,5 +1,23 @@
+import { fetchWithoutToken } from "../helpers/fetch";
+import { types } from "../types/types";
+
 export const startLogin = ( email, password ) => {
-  return async () => {
-    console.log(email, password);
+  return async ( dispatch ) => {
+    const resp = await fetchWithoutToken('auth', { email, password }, 'POST');
+    const decodedData = await resp.json();
+    
+    if( decodedData.ok ) {
+      localStorage.setItem( 'token', decodedData.token );
+      localStorage.setItem( 'token-init-date', new Date().getTime() );
+      dispatch( login({
+        uid: decodedData.uid,
+        name: decodedData.name,
+      }));
+    }
   }
 };
+
+const login = ( user ) => ({
+  type: types.authLogin,
+  payload: user,
+});
--- 09-calendar-app/src/reducers/authReducer.js
+++ 09-calendar-app/src/reducers/authReducer.js
@@ -1,9 +1,17 @@
+import { types } from "../types/types";
+
 const initState = {
   checking: true,
 }
 
 export const authReducer = ( state = initState, action ) => {
   switch (action.type) {
+    case types.authLogin:
+      return {
+        ...state,
+        checking: false,
+        ...action.payload
+      }
     default:
       return state;
   }
