dc19fe211aee8dec0dd7365e82c666035ffcbb23
CesarArellano <raywayday@gmail.com>
2021-10-31 14:27:56 -0600

    Adding try and catch

--- 09-calendar-app/src/actions/auth.js
+++ 09-calendar-app/src/actions/auth.js
@@ -41,20 +41,26 @@ export const startRegister = ( email, password, name ) => {
 
 export const startChecking = () => {
   return async (dispatch) => {
-    const resp = await fetchWithToken('auth/renew');
-    const decodedData = await resp.json();
-    
-    if( decodedData.ok ) {
-      localStorage.setItem( 'token', decodedData.token );
-      localStorage.setItem( 'token-init-date', new Date().getTime() );
-      dispatch( login({
-        uid: decodedData.uid,
-        name: decodedData.name,
-      }));
-    } else {
-      Swal.fire('Error', decodedData.msg, 'error');
-      dispatch( checkingFinish() )
+    try {
+      const resp = await fetchWithToken('auth/renew');
+      const decodedData = await resp.json();
+      console.log(decodedData);
+      if( decodedData.ok ) {
+        localStorage.setItem( 'token', decodedData.token );
+        localStorage.setItem( 'token-init-date', new Date().getTime() );
+        dispatch( login({
+          uid: decodedData.uid,
+          name: decodedData.name,
+        }));
+      } else {
+        Swal.fire('Error', decodedData.msg, 'error');
+        dispatch( checkingFinish() );
+      }
+    } catch(e) {
+      Swal.fire('Error', e, 'error');
+      dispatch( checkingFinish() );
     }
+    
   }
 }
 
