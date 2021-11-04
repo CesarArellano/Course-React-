942b6f8f41eb5e8764a3ff56a25bc129ecd291ef
CesarArellano <raywayday@gmail.com>
2021-11-04 15:26:52 -0600

    Adding test with incorrect login

--- 09-calendar-app/src/test/actions/auth.test.js
+++ 09-calendar-app/src/test/actions/auth.test.js
@@ -1,8 +1,15 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
+
 import '@testing-library/jest-dom';
+
 import { startLogin } from '../../actions/auth';
 import { types } from '../../types/types';
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -36,6 +43,17 @@ describe('Testing with auth actions', () => {
     
     const token = localStorage.setItem.mock.calls[0][1];
     console.log( token );
-  })
+  });
+
+  test('starLogin incorrect', async () => {
+    await store.dispatch( startLogin('cesarmauricio.arellano@gmail.com', '12345678') );
+    let actions = store.getActions();
+    expect( actions ).toEqual([]);
+    expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Usuario y/o contraseña son incorrectos ', 'error');
+    await store.dispatch( startLogin('cesarmauricio.arellano2@gmail.com', '123456') );
+    actions = store.getActions();
+    expect( Swal.fire ).toHaveBeenCalledWith('Error', 'El usuario no existe con ese correo.', 'error');
+  });
+  
   
 })
