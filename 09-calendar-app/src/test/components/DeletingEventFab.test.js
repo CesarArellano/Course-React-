6769936cdf257073ca3e3437c62d27dd839d5e87
CesarArellano <raywayday@gmail.com>
2021-11-04 22:16:04 -0600

    Testing with dispatch fix

--- 09-calendar-app/src/test/components/DeletingEventFab.test.js
+++ 09-calendar-app/src/test/components/DeletingEventFab.test.js
@@ -1,11 +1,17 @@
+import '@testing-library/jest-dom';
+
 import React from 'react';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { mount } from 'enzyme'
-
-import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
+
 import { DeletingEventFab } from '../../components/ui/DeletingEventFab';
+import { eventStartDelete } from '../../actions/events';
+
+jest.mock('../../actions/events',() => ({
+  eventStartDelete: jest.fn()
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -25,5 +31,10 @@ describe('Testing with DeletingEventFab Component', () => {
   test('It should show correctly', () => {
     expect( wrapper ).toMatchSnapshot();
   });
+  
+  // test('It should call the eventStartDelete function when doing click', () => {
+  //   wrapper.find('button').prop('onClick')();
+  //   expect( eventStartDelete ).toHaveBeenCalled();
+  // });
   
 });
