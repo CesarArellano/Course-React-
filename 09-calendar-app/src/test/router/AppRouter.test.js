0d68cf688bbf9bf34f7e1a6d816858df6957b189
CesarArellano <raywayday@gmail.com>
2021-11-04 22:37:16 -0600

    Adding new testing in AppRouter

--- 09-calendar-app/src/test/router/AppRouter.test.js
+++ 09-calendar-app/src/test/router/AppRouter.test.js
@@ -10,23 +10,70 @@ import { AppRouter } from '../../routers/AppRouter';
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
 
-const initState = { 
-  auth: {
-    checking: true
-  }
-};
-
-const store = mockStore( initState );
-
 describe('Testing in <AppRouter />', () => {
   test('It should show waiting message', () => {
+    const initState = { 
+      auth: {
+        checking: true
+      }
+    };
+    
+    const store = mockStore( initState );
+
     const wrapper = mount(
       <Provider store={ store }>
         <AppRouter />
       </Provider>
     );
-    
+
     expect( wrapper ).toMatchSnapshot();
     expect( wrapper.find('.utils__spinner').exists() ).toBe(true);
-  })
+  });
+
+  test('It should show the public route', () => {
+    const initState = { 
+      auth: {
+        uid: null,
+        checking: false
+      }
+    };
+    
+    const store = mockStore( initState );
+    
+    const wrapper = mount(
+      <Provider store={ store }>
+        <AppRouter />
+      </Provider>
+    );
+
+    expect( wrapper ).toMatchSnapshot();
+    expect( wrapper.find('.login-container').exists() ).toBe(true);
+  });
+
+  test('It should show the public route', () => {
+    const initState = {
+      ui: {
+        modalOpen: false,
+      },
+      calendar: {
+        events: []
+      },
+      auth: {
+        uid: '12345',
+        name: 'Jorge',
+        checking: false
+      }
+    };
+    
+    const store = mockStore( initState );
+    
+    const wrapper = mount(
+      <Provider store={ store }>
+        <AppRouter />
+      </Provider>
+    );
+
+    expect( wrapper ).toMatchSnapshot();
+    expect( wrapper.find('.calendar-screen').exists() ).toBe(true);
+  });
 });
