cdb585ad372e71b0c30606f731bfa265f9e00d8d
CesarArellano <raywayday@gmail.com>
2021-11-04 23:01:54 -0600

    Testing with LoginScreen component

--- 09-calendar-app/src/test/components/LoginScreen.test.js
+++ 09-calendar-app/src/test/components/LoginScreen.test.js
@@ -1,17 +1,24 @@
 import '@testing-library/jest-dom';
 
 import React from 'react';
+import Swal from 'sweetalert2';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { mount } from 'enzyme'
 import { Provider } from 'react-redux';
 
 import { LoginScreen } from '../../components/auth/LoginScreen';
-import { startLogin } from '../../actions/auth';
+import { startLogin, startRegister } from '../../actions/auth';
 
 jest.mock('../../actions/auth', () => ({
-  startLogin: jest.fn()
+  startLogin: jest.fn(),
+  startRegister: jest.fn()
 }));
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
+}));
+
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -28,12 +35,17 @@ const wrapper = mount(
 );
 
 describe('Testing with <LoginScreen />', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks(); // Limpieza de mocks antes de cada test
+  });
+
   test('should show correctly', () => {
     expect( wrapper ).toMatchSnapshot();
   });
   
   test('should call dispatch to login', () => {
-    
+
     wrapper.find('input[name="lEmail"]').simulate('change', {
       target: {
         name: 'lEmail',
@@ -54,4 +66,52 @@ describe('Testing with <LoginScreen />', () => {
 
     expect( startLogin ).toHaveBeenCalledWith('raywayday@gmail.com', '123456');
   });
+
+  test('No registration if passwords are different', () => {
+    
+    wrapper.find('input[name="rPassword"]').simulate('change', {
+      target: {
+        name: 'rPassword',
+        value: '1234567',
+      }
+    });
+
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '123456',
+      }
+    });
+
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault(){}
+    });
+    
+    expect( startRegister ).not.toHaveBeenCalled();
+    expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Las contraseñas deben ser iguales', 'error');
+  });
+
+  test('should call startRegister', () => {
+    wrapper.find('input[name="rPassword"]').simulate('change', {
+      target: {
+        name: 'rPassword',
+        value: '123456',
+      }
+    });
+
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '123456',
+      }
+    });
+
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault(){}
+    });
+
+    expect( Swal.fire ).not.toHaveBeenCalled();
+    expect( startRegister ).toHaveBeenCalled();
+  });
+  
 });
