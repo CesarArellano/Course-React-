4e440bef73bcd4a64e81e615a44165ef08c6280f
CesarArellano <raywayday@gmail.com>
2022-06-17 00:43:37 -0500

    Finished design of JournalApp

--- 08-mui-journal-app/src/journal/pages/JournalPage.tsx
+++ 08-mui-journal-app/src/journal/pages/JournalPage.tsx
@@ -1,10 +1,27 @@
-import { Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout';
 import { NothingSelectedView, NoteView } from '../views';
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
+      <>
+        <NoteView />
+        <IconButton
+          size='large'
+          sx={{
+            transition: '0.8s all ease-out',
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': {  backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 20,
+            bottom: 20
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }}/>
+        </IconButton>
+      </>
     </JournalLayout>
   )
 }
