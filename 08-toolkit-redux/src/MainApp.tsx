76486b226362f5e1fe80c8a54e7d7b8e7a7d873e
CesarArellano <raywayday@gmail.com>
2022-07-10 21:01:48 -0500

    Knowing RTK Query

--- 08-toolkit-redux/src/MainApp.tsx
+++ 08-toolkit-redux/src/MainApp.tsx
@@ -1,11 +1,12 @@
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { PokemonApp } from './components/PokemonApp';
+import { TodoApp } from './components/TodoApp';
 
 export const MainApp = () => {
   return (
     <Provider store={ store }>
-      <PokemonApp />
+      <TodoApp />
     </Provider>
   )
 }
