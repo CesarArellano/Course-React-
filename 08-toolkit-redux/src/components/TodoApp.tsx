00539d568a3ea28a8f24f4ef2ecd8b269fcff3eb
CesarArellano <raywayday@gmail.com>
2022-07-10 21:40:45 -0500

    Finished RTK section

--- 08-toolkit-redux/src/components/TodoApp.tsx
+++ 08-toolkit-redux/src/components/TodoApp.tsx
@@ -1,15 +1,65 @@
-import React from 'react'
+import { useState } from 'react';
+import { useGetTodoQuery } from '../store/apis'
+
+// export const TodoApp = () => {
+//   const { data: todos = [], isLoading } = useGetTodosQuery();
+  
+//   return (
+//     <>
+//       <h1>Todos - RTK - Query</h1>
+//       <hr />
+//       <h4>isLoading { isLoading ? 'True' : 'False' }</h4>
+//       <pre>...</pre>
+//       <ul>
+//         {
+//           todos.map( ({ id, title, completed }) => (
+//             <li key={ id }>
+//               <p><strong> { completed ? 'DONE' : 'PENDING' }</strong></p>
+//               { title }
+//             </li>
+//           ))
+//         }
+//       </ul>
+//       <button>
+//         New Todo
+//       </button>
+//     </>
+//   )
+// }
+
 
 export const TodoApp = () => {
+  const [todoId, setTodoId] = useState<number>(1)
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+  
+  const nextTodo = () => {
+    setTodoId(todoId + 1 );
+  }
+  
+  const prevTodo = () => {
+    if( todoId == 1 ) return;
+    setTodoId(todoId - 1 );
+  }
+
   return (
     <>
       <h1>Todos - RTK - Query</h1>
       <hr />
-      <h4>isLoading...</h4>
-      <pre>...</pre>
-      <button>
-        New Todo
+      <h4>isLoading { isLoading ? 'True' : 'False' }</h4>
+      <pre>
+        { JSON.stringify( todo ) }
+      </pre>
+      <button
+        onClick={ prevTodo }
+      >
+        Prev Todo
+      </button>
+      <button
+        onClick={ nextTodo }
+      >
+        Next Todo
       </button>
     </>
   )
 }
+
--- 08-toolkit-redux/src/store/apis/todosApi.ts
+++ 08-toolkit-redux/src/store/apis/todosApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { TodoInterface } from '../../interfaces/todo_interface';
 
 export const todosApi = createApi({
   reducerPath: 'todos',
@@ -6,10 +7,13 @@ export const todosApi = createApi({
     baseUrl: 'https://jsonplaceholder.typicode.com'
   }),
   endpoints: (builder) => ({
-    getTodos: builder.query({
+    getTodos: builder.query<Array<TodoInterface>, void>({
       query: () => '/todos'
-    })
+    }),
+    getTodo: builder.query<TodoInterface, number>({
+      query: (todoId) => `/todos/${ todoId }`
+    }),
   })
 })
 
-export const { } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
